ae7f5747fab82164b1ff505afb318a672a92ee43
llk214 <llk40214@gmail.com>
2026-04-11 14:32:06 +0800

    Remove unused assertThrows import in ExportCommandTest

--- src/test/java/seedu/RLAD/command/ExportCommandTest.java
+++ src/test/java/seedu/RLAD/command/ExportCommandTest.java
@@ -17,7 +17,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ExportCommandTest {
